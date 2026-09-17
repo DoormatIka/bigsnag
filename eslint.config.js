0cc38458578382ba628dad0cd5f24dd8c07d3b8a
DoormatIka <yomotouhou@gmail.com>
2026-09-17 22:29:31 +0800

    added eslint to catch floating promises

---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
+    },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error",
+      "@typescript-eslint/no-misused-promises": "error",
+    },
+  },
+];
